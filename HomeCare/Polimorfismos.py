7b34bbbbcf90442c20f1ab86c9661935a4fe1f7a
LuizaProfitti <luizaprofittii@gmail.com>
2024-03-04 19:50:15 -0300

    Criação Polimorfismos
    Criação da conexão do banco de dados

---
+++ HomeCare/Polimorfismos.py
@@ -0,0 +1,22 @@
+#Conexão com o banco de dados
+import mysql.connector as sql
+
+def conexao_bd():
+    conexao = sql.connect(
+        host='localhost',
+        user= 'root',
+        password='1234',
+        database='bd_home_care'
+    )
+
+    return conexao
+
+#Presets de banco de dados
+
+#Notificações padrão
+
+#Notificações de erro
+
+
+
+Fluxograma
